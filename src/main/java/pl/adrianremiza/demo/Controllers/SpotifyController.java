c1495f3a80112b40c04bb3bcdb7eb991ffe391c9
macbookadrian <a.grenda@onet.eu>
2021-07-16 15:08:23 +0200

    updatee

--- src/main/java/pl/adrianremiza/demo/Controllers/SpotifyController.java
+++ src/main/java/pl/adrianremiza/demo/Controllers/SpotifyController.java
@@ -190,7 +190,7 @@ public class SpotifyController {
     @GetMapping("/song/queue/skipvote")
     public Object addToCounterToSkipVote() throws InterruptedException {
         System.out.println("counterVote: "+trackService.getCounterSkipVote());
-            if(trackService.getCounterSkipVote()>2){fsdgsdsdggsdgs
+            if(trackService.getCounterSkipVote()>2){
                 System.out.println(trackService.getCounterSkipVote()+"2");
                 if(this.trackService.getTracksQueue().size()==0){
                     System.out.println(trackService.getCounterSkipVote()+"3");
