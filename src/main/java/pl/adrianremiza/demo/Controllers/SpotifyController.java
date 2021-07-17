7c9582d90cb558f2ca730335d353681c88693966
Kinder-ad <53996126+Kinder-ad@users.noreply.github.com>
2021-07-17 22:20:58 +0200

    Update SpotifyController.java

--- src/main/java/pl/adrianremiza/demo/Controllers/SpotifyController.java
+++ src/main/java/pl/adrianremiza/demo/Controllers/SpotifyController.java
@@ -193,7 +193,7 @@ public class SpotifyController {
     @GetMapping("/song/queue/skipvote")
     public Object addToCounterToSkipVote() throws InterruptedException {
         System.out.println("counterVote: "+trackService.getCounterSkipVote());
-            if(trackService.getCounterSkipVote()>13){
+            if(trackService.getCounterSkipVote()>3){
                 System.out.println(trackService.getCounterSkipVote()+"2");
                 if(this.trackService.getTracksQueue().size()==0){
                     System.out.println(trackService.getCounterSkipVote()+"3");
